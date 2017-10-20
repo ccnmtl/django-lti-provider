6a7ff33e069120bd4c2194e8f393d49a1540c2f7
Susan Dreher <sdreher@columbia.edu>
2017-10-20 10:15:45 -0400

    Refactor auth failure and course configuration paths
    Both paths can be handled more cleanly with redirect logic.

---
+++ lti_provider/mixins.py
@@ -0,0 +1,52 @@
+from django.conf import settings
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+from django.http.response import HttpResponseRedirect
+from lti_provider.lti import LTI
+from lti_provider.models import LTICourseContext
+
+
+class LTIAuthMixin(object):
+    role_type = 'any'
+    request_type = 'any'
+
+    def join_groups(self, request, lti, ctx):
+        # add the user to the requested groups
+        request.user.groups.add(ctx.group)
+        for role in lti.user_roles(request):
+            role = role.lower()
+            if ('staff' in role or
+                'instructor' in role or
+                    'administrator' in role):
+                request.user.groups.add(ctx.faculty_group)
+                break
+
+    def course_configuration(self, request, lti):
+        # check if course is configured
+        if settings.LTI_TOOL_CONFIGURATION['course_aware']:
+            ctx = LTICourseContext.objects.get(
+                lms_course_context=lti.course_context(request))
+
+            # add user to the course
+            self.join_groups(request, lti, ctx)
+
+    def dispatch(self, request, *args, **kwargs):
+        lti = LTI(self.request_type, self.role_type)
+
+        # validate the user via oauth
+        user = authenticate(request=request, lti=lti)
+        if user is None:
+            lti.clear_session(request)
+            return HttpResponseRedirect(reverse('lti-fail-auth'))
+
+        # login
+        login(request, user)
+
+        # configure course groups if requested
+        try:
+            self.course_configuration(request, lti)
+        except (KeyError, ValueError, LTICourseContext.DoesNotExist):
+            return HttpResponseRedirect(reverse('lti-course-config'))
+
+        self.lti = lti
+        return super(LTIAuthMixin, self).dispatch(request, *args, **kwargs)
