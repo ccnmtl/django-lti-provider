c52690eecae94a30fb1c95cb45d2249b4919c801
ndittren <36773036+ndittren@users.noreply.github.com>
2021-11-11 11:29:29 -0500

    fix flake8 errors

--- lti_provider/urls.py
+++ lti_provider/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
     re_path(r'^config.xml$', LTIConfigView.as_view(), {}, 'lti-config'),
     re_path(r'^auth$', LTIFailAuthorization.as_view(), {}, 'lti-fail-auth'),
     re_path(r'^course/config$',
-        LTICourseConfigure.as_view(), {}, 'lti-course-config'),
+            LTICourseConfigure.as_view(), {}, 'lti-course-config'),
     re_path(r'^course/enable/$',
-        LTICourseEnableView.as_view(), {}, 'lti-course-enable'),
+            LTICourseEnableView.as_view(), {}, 'lti-course-enable'),
     re_path(r'^landing/$', LTILandingPage.as_view(), {}, 'lti-landing-page'),
     re_path('^grade/$', LTIPostGrade.as_view(), {}, 'lti-post-grade'),
     re_path(r'^$', LTIRoutingView.as_view(), {}, 'lti-login'),
     re_path(r'^assignment/(?P<assignment_name>.*)/(?P<pk>\d+)/$',
-        LTIRoutingView.as_view(), {}, 'lti-assignment-view'),
+            LTIRoutingView.as_view(), {}, 'lti-assignment-view'),
     re_path(r'^assignment/(?P<assignment_name>.*)/$',
-        LTIRoutingView.as_view(), {}, 'lti-assignment-view')
+            LTIRoutingView.as_view(), {}, 'lti-assignment-view')
 ]
