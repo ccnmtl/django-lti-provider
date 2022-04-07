fea52c4ec0149afa125eaa734e4a567c525b4f82
Susan Dreher <sdreher@gmail.com>
2022-04-06 20:52:15 -0400

    Release prep 1.0.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-lti-provider",
-    version="0.4.7",
+    version="1.0.0",
     author="Susan Dreher",
     author_email="sdreher@columbia.edu",
     url="https://github.com/ccnmtl/django-lti-provider",
